1e88d7a35dd3e44c317886a8250a21663e0464a4
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-30 16:58:00 -0300

    empacado no vs2022

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, 
+// faça um programa, na linguagem que desejar, que calcule e retorne:
+// • O menor valor de faturamento ocorrido em um dia do mês;
+// • O maior valor de faturamento ocorrido em um dia do mês;
+// • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+// IMPORTANTE:
+// a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
+// b) Podem existir dias sem faturamento, como nos finais de semana e feriados. 
+// Estes dias devem ser ignorados no cálculo da média;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\dados.json");
+
+            List<Faturamento> faturamento = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+
+            //Console.WriteLine(json);
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
---
+++ ConsoleApp1/Serialization/Faturamento.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Serialization
+{
+    public class Faturamento
+    {
+        [JsonProperty("dia")]
+        public int Dia { get; set; }
+
+        [JsonProperty("valor")]
+        public double Valor { get; set; }
+
+    }
+}
